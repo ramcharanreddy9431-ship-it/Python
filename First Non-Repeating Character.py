1ee4cf5040a5c3a1e35d8cde43333a6b7f74905a
ramcharanreddy9431-ship-it <ramcharanreddy9431@gmail.com>
2026-06-22 19:11:55 +0530

    Update First Non-Repeating Character.py

--- First Non-Repeating Character.py
+++ First Non-Repeating Character.py
@@ -1,5 +1,5 @@
 def first_non_repeating(s):
     for c in s:
-        if s.lower().count(c.lower()) == 1:
+        if s.lower().count(c.lower()) == 1:   #checks the number of times each character occurs in a string s and checks the condition if it occurs once
             return c
     return ""
